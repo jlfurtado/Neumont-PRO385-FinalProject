0f1e5935279545aad776aa13944950693954c1a9
Michael Vanderlip <mvanderlip@student.neumont.edu>
2017-11-15 14:23:59 -0700

    Added controller support, fixed various bugs

--- Project/Assets/Scripts/Player/PlayerMovement.cs
+++ Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public float m_torqueForce;
     public float m_moveForce;
     public float m_jumpForce;
+    [SerializeField, Range(0.0f, 1.0f)] float m_movePenaltyCap;
 
     // Use this for initialization
     void Start()
@@ -22,12 +23,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        m_rigidbody.angularVelocity = new Vector3(0.0f, m_torqueForce * Input.GetAxis("Horizontal"), 0.0f);
-        m_rigidbody.velocity = new Vector3(transform.forward.x * m_moveForce * -Input.GetAxis("Vertical"), m_rigidbody.velocity.y, transform.forward.z * m_moveForce * -Input.GetAxis("Vertical"));
+        m_rigidbody.angularVelocity = new Vector3(0.0f, m_torqueForce * Input.GetAxis("Rotate"), 0.0f);
+        Vector3 direction = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized;
+        float compensatedMove = m_moveForce * Mathf.Clamp(Vector3.Dot(transform.forward, direction), m_movePenaltyCap, 1.0f);
+        m_rigidbody.velocity = new Vector3(direction.x * compensatedMove, m_rigidbody.velocity.y, direction.z * compensatedMove);
+
         if (m_jumpCheck.grounded)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetAxis("Jump") != 0.0f)
             {
+                m_rigidbody.velocity = new Vector3(m_rigidbody.velocity.x, 0.0f, m_rigidbody.velocity.z);
                 m_rigidbody.AddForce(Vector3.up * m_jumpForce);
             }
         }
