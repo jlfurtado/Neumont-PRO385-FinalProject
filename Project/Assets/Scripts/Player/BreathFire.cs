c71ba5ae0b93fca46f4ee18b417bb68fae7d9e9f
mvanderlip <mvanderlip@student.neumont.edu>
2017-11-21 09:49:39 -0700

    Added Fire Breath

---
+++ Project/Assets/Scripts/Player/BreathFire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathFire : MonoBehaviour {
+
+    [SerializeField] private string m_fireAxis;
+    [SerializeField] private ParticleSystem m_breath;
+    private float m_maxEmission;
+
+	// Use this for initialization
+	void Start () {
+        m_maxEmission = m_breath.emission.rateOverTimeMultiplier;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var emitter = m_breath.emission;
+        Debug.Log(Input.GetAxis(m_fireAxis));
+        emitter.rateOverTime = Input.GetAxis(m_fireAxis) * m_maxEmission;
+	}
+}
