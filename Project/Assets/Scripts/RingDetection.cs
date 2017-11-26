f1f8c99003761e296254bd5732213f7d4d76e510
Michael Vanderlip <mvanderlip@student.neumont.edu>
2017-11-26 08:57:19 -0700

    Added Ring Detection, replace the unity event callback with whatever our counting system will be (i.e. don't use it, it was just for testing, unless using it makes it easier of course) WARNING: Adjusted layers

---
+++ Project/Assets/Scripts/RingDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RingDetection : MonoBehaviour {
+
+    [SerializeField] private UnityEvent m_callback;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_callback.Invoke();
+        MeshCollider[] meshColliders = gameObject.GetComponents<MeshCollider>();
+        for (int k = 0; k < meshColliders.Length; k++)
+        {
+            if (meshColliders[k].isTrigger)
+            {
+                meshColliders[k].enabled = false;
+            }
+        }
+    }
+
+    public void TestCallback()
+    {
+        Debug.Log("Hello Callback");
+    }
+}
