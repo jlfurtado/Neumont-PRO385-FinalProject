a78c726ab3c63dd8c358decbb5649eaca65258d1
Justin Furtado <justin.l.furtado@gmail.com>
2017-11-27 09:44:39 -0700

    Fix bugs

--- Project/Assets/Scripts/Difficulty.cs
+++ Project/Assets/Scripts/Difficulty.cs
@@ -79,7 +79,8 @@ public class Difficulty : MonoBehaviour {
     private void CreateWinConditionFromDifficulty()
     {
         int m_total = m_minObjective + m_difficulty * m_objectsPerDifficulty;
-        m_treesNeeded = 1 + rand.Next(0, m_total - 1);
+        int m_minTrees = Mathf.Min((m_total / 2) + 1, m_total - 1);
+        m_treesNeeded = m_minTrees + rand.Next(0, m_total - m_minTrees);
         m_ringsNeeded = m_total - m_treesNeeded;
         StartTimer();
         SetObjectiveText();
