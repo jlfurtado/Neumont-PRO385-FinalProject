e13b4d250431a58aabd7f1566750782229623968
mvanderlip <mvanderlip@student.neumont.edu>
2017-12-03 15:20:40 -0700

    Leash ParticleFX

--- Project/Assets/Scripts/AttachTwoPoints.cs
+++ Project/Assets/Scripts/AttachTwoPoints.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class AttachTwoPoints : MonoBehaviour {
     [SerializeField] private GameObject m_obj1;
     [SerializeField] private GameObject m_obj2;
+    [SerializeField] private ParticleSystem m_tetherParticle;
     [SerializeField] private Vector3 m_offset;
     [SerializeField] private float m_lowDist;
     [SerializeField] private float m_highDist;
     [SerializeField] private Color m_lowColor;
     [SerializeField] private Color m_highColor;
+    [SerializeField] private float m_modEmit;
+    [SerializeField] private float m_baseEmit;
 
     private Renderer m_renderer;
 
     private void Awake()
     {
         m_renderer = GetComponent<Renderer>();
+
     }
 
     // Update is called once per frame
@@ -27,15 +31,23 @@ public class AttachTwoPoints : MonoBehaviour {
 
         bool show = toObj2.magnitude > m_lowDist;
         m_renderer.enabled = show;
-
+        m_tetherParticle.gameObject.SetActive(show);
         // if player going away from other player AND player distance > thresholdDistance
         if (show)
         {
             float interp = (toObj2.magnitude - m_lowDist) / (m_highDist - m_lowDist);
             transform.position = (p1 + p2) / 2.0f;
-            transform.rotation = Quaternion.LookRotation(Vector3.Cross(toObj2.normalized, Vector3.up), toObj2.normalized);
+            Quaternion interpRot = Quaternion.LookRotation(Vector3.Cross(toObj2.normalized, Vector3.up), toObj2.normalized);
+            transform.rotation = interpRot;
             transform.localScale = new Vector3(transform.localScale.x, ((toObj2.magnitude - 1.0f) * 0.49f), transform.localScale.z);
-            m_renderer.material.color = Color.Lerp(m_lowColor, m_highColor, interp);
+            Color interpColor = Color.Lerp(m_lowColor, m_highColor, interp);
+            m_renderer.material.color = interpColor;
+            var main = m_tetherParticle.main;
+            main.startColor = new Color(interpColor.r, interpColor.g, interpColor.b, 1.0f);
+            var emit = m_tetherParticle.emission;
+            emit.rateOverTime = (interp * m_modEmit) + m_baseEmit;
+            var shape = m_tetherParticle.shape;
+            shape.radius = toObj2.magnitude / 2.0f;
         }
     }
 }
